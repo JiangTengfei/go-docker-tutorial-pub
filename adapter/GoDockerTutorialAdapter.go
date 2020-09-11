06a53aad9c884272fc235991d797665bb42a3251
jiangtengfei2261 <jiangtengfei2261@ipalfish.com>
2020-09-11 16:15:41 +0800

    init

---
+++ adapter/GoDockerTutorialAdapter.go
@@ -0,0 +1,35 @@
+package GoDockerTutorialAdapter
+
+import (
+	"context"
+	pb "github.com/jiangtengfei/go-docker-tutorial-pub/grpc"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+const (
+	address = "172.17.0.2:9090"
+)
+
+var (
+	c pb.GreeterClient
+)
+
+func init() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	c = pb.NewGreeterClient(conn)
+}
+
+func SayHello(ctx context.Context, req *pb.HelloRequest) *pb.HelloReply {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	rr, err := c.SayHello(ctx, &pb.HelloRequest{Name: "JTF"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	return rr
+}
