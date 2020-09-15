0b505ab908ce84f1d05a86da55ed87484c7228d9
jiangtengfei2261 <jiangtengfei2261@ipalfish.com>
2020-09-15 14:35:42 +0800

    edit

--- adapter/GoDockerTutorialAdapter.go
+++ adapter/GoDockerTutorialAdapter.go
@@ -17,7 +17,7 @@ var (
 
 func init() {
 	etcdClient, _ = clientv3.New(clientv3.Config{
-		Endpoints:   []string{"127.0.0.1:2379"},
+		Endpoints:   []string{"192.168.145.180:2379"},
 		DialTimeout: 5 * time.Second,
 	})
 
